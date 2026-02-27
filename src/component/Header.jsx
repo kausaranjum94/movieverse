4f8175e564d6bf59f15c5f5c0c6c7692258bd579
Kausar <anjumkausar94@gmail,com>
2026-02-27 16:15:12 +0530

    Logo Url fixed

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import { FaHeart } from "react-icons/fa6";
+import logo from "../assets/logo.png";
 
 export const Header = () => {
   return (
     <header className="bg-gray-800 text-white p-3 grid grid-cols-12 gap-4 justify-between items-center rounded-xl">
       <div className="appBranding col-span-3 lg:col-span-3 md:col-span-4 sm:col-span-6">
-        <img src="../src/assets/logo.png" alt="Logo" />
+        <img src={logo} alt="Logo" />
       </div>
       <div className="appNavbar col-span-9 lg:col-span-9 md:col-span-8 sm:col-span-6 flex align-center justify-end gap-4">
         <nav className="space-x-6 text-md text-white">
